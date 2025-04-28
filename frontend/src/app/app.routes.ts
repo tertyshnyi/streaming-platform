bebc3337533971efb3944a32aa0706d7be84607b
volodymyr.tertyshnyi <volodymyr.tertyshnyi@student.tuke.sk>
2025-04-28 21:57:23 +0200

    Updated design of the 404 page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,6 @@ import {AuthGuard} from './core/guards/auth.guard';
 export const routes: Routes = [
   {path: 'user-settings', component: UserSettings, canActivate: [AuthGuard]},
 
-  {path: '', redirectTo: '', pathMatch: 'full'},
+  // {path: '', redirectTo: '', pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
 ];
