d5453dad3281d03b079d023e294afbbf1bc5d957
volodymyr.tertyshnyi <volodymyr.tertyshnyi@student.tuke.sk>
2025-05-07 02:28:40 +0200

    Updated

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   keyCloakUrl: '/auth',
-  beUrl: 'http://20.166.32.72',
-  appUrl: 'http://20.166.32.72',
+  beUrl: 'http://20.166.32.72.nip.io/streaming-platform',
+  appUrl: 'http://20.166.32.72.nip.io',
 };
