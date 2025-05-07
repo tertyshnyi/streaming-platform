b31ed3bfe2e4c7894c5bb956e0f79e7dc4de981d
volodymyr.tertyshnyi <volodymyr.tertyshnyi@student.tuke.sk>
2025-05-07 19:52:02 +0200

    Changed app to streaming-platform

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   keyCloakUrl: 'https://20.166.32.72.nip.io/auth',
-  beUrl: 'https://20.166.32.72.nip.io/app',
+  beUrl: 'https://20.166.32.72.nip.io/streaming-platform',
   appUrl: 'https://20.166.32.72.nip.io',
 };
