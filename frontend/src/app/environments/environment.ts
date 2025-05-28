ad07866992f3fac72c5235912e893e37b2397bea
volodymyr.tertyshnyi <volodymyr.tertyshnyi@student.tuke.sk>
2025-05-28 22:38:45 +0200

    changed to prod env

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,11 +1,11 @@
-// export const environment = {
-//   keyCloakUrl: 'https://20.166.32.72.nip.io/auth',
-//   beUrl: 'https://20.166.32.72.nip.io/streaming-platform',
-//   appUrl: 'https://20.166.32.72.nip.io',
-// };
 export const environment = {
-  keyCloakUrl: 'http://localhost:8081',
-  beUrl: 'http://localhost:8080',
-  appUrl: 'http://localhost:4200',
+  keyCloakUrl: 'https://20.166.32.72.nip.io/auth',
+  beUrl: 'https://20.166.32.72.nip.io/streaming-platform',
+  appUrl: 'https://20.166.32.72.nip.io',
 };
+// export const environment = {
+//   keyCloakUrl: 'http://localhost:8081',
+//   beUrl: 'http://localhost:8080',
+//   appUrl: 'http://localhost:4200',
+// };
 
